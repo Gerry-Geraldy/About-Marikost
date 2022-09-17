0ec2e341f8da70f3f0bdb193b1e8e21e317fa26c
Gerry-Geraldy <whateveritakes10@gmail.com>
2022-09-17 12:53:31 +0800

    about marikost

---
+++ about.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>About MariKost</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <img src="img/10097.jpg" alt="" class="img-fluid">
+
+        <div class="row mt-5">
+            <div class="col-5">
+                <img src="img/Business team looking for new people.jpg" class="img-thumbnail" alt="...">
+            </div>
+            <div class="col-7">
+                <h1>WHAT IS MARIKOST</h1>
+                <p>Platform layanan jasa yang membantu mengatasi masalah pemilik kost, pencari kost bahkan anak kost untuk mengelola, mencari kost hingga memenuhi kebutuhan selama menjadi anak kost. Marikost juga memberikan rekomendasi kost-kostan yang sesuai dengan kategori yang tersedia dan dilengkapi dengan deskripsi yang rinci dan lengkap. Bukan hanya itu saja, Marikost menyediakan berbagai fitur layanan jasa yang dapat membantu pengguna terutama anak kost untuk memenuhi kebutuhan sehari-hari. Gak perlu khawatir dalam menggunakannya karena fitur-fitur yang Marikost sediakan sangat user friendly.
+                </p>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-7">
+                <br><br>
+                <h1>MARIKOST GOALS</h1>
+                <p>Tujuan dari Marikost sendiri sederhana yaitu ingin menyediakan suatu platform layanan jasa berbasis teknologi yang mudah digunakan sehingga pengguna seperti pemilik kost, pencari kost hingga anak kost dapat menggunakan satu platform ini untuk mengatasi segala permasalahan yang mereka hadapi.
+                </p>
+            </div>
+            <div class="col-5">
+                <img src="img/marikost.PNG" alt="" class="img-thumbnail">
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-5">
+                <img src="img/4318588.jpg" class="img-thumbnail" alt="...">
+            </div>
+            <div class="col-7">
+                <br><br>
+                <h1>OUR UNIQUENESS</h1>
+                <p>Yang membedakan Marikost dengan platform lain salah satunya adalah fitur layanan jasa seperti jasa tukang, jasa laundry, pembayaran, hingga toko kelontong yang di sediakan dalam satu platform. Sehingga, pengguna khususnya pemilik kost dan anak kost tidak perlu khawatir dalam memenuhi kebutuhan sehari-hari selama menjadi anak kost. Marikost siap membantu kalian!
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
